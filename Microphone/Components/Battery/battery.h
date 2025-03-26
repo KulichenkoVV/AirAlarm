82b429f51b8260ad2192fa5f2102f4228ccc8766
DmytroZaiats <dima.mladshiy@gmail.com>
2025-03-26 11:55:49 +0200

    Added battery code

--- Microphone/Components/Battery/battery.h
+++ Microphone/Components/Battery/battery.h
@@ -1,10 +1,18 @@
-
 #ifndef BATTERY_H
 #define BATTERY_H
 
 #include <stdint.h>
 
-void bat_init(void);
-void bat_get_data(uint8_t *data, uint32_t size_data);
+// Enumerator for battery charge levels
+typedef enum {
+    FULL_POWER,       // 50-100% charge
+    MODERATE_POWER,   // 20-50% charge
+    LOW_POWER,        // 10-20% charge
+    CRITICAL_POWER    // Below 10% charge
+} BatteryChargeLevel;
+
+// Function declarations
+void Battery_Init(void);
+BatteryChargeLevel Battery_GetChargeLevel(void); // Returns the charge level enum
 
 #endif // BATTERY_H
